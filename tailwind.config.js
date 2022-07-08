41ab692d2de65ad44fdec8cec517f34197b01071
sudo-chelsea <chelseakowouvi@gmail.com>
2022-07-08 03:13:58 +0000

    data aos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 // tailwind.config.js
-module.exports = {
-    theme: {
-      colors: {
-        'indigo-lighter': '#b3bcf5',
-        'indigo': '#5c6ac4',
-        'indigo-dark': '#202e78',
-      }
-    }
-  }
+// module.exports = {
+//     theme: {
+//       colors: {
+//         'indigo-lighter': '#b3bcf5',
+//         'indigo': '#5c6ac4',
+//         'indigo-dark': '#202e78',
+//       }
+//     }
+//   }
